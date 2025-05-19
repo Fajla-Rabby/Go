bdbd166463f4e24e68ef09991a01817b58597a0b
Fajla-Rabby <fajla.rabby008@gmail.com>
2025-05-19 23:44:21 +0600

    functions

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import "fmt"
 
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+	return sum
+}
+
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2 //31
+	mul := num1 * num2 //210
+
+	return sum, mul
+}
+
 func main() {
-	/*
-		int
-		float32
-		bool
-		string
-	*/
-	var a int = 10
-	fmt.Println(a)
+	a := 10
+	b := 21
+	// sum := add(a, b)
+	p, q := getNumbers(a, b)
+
+	fmt.Println(p)
+	fmt.Println(q)
 }
